82ab8ffb3ef52d7c681381ec64a9bf821b5affb6
yibeichan <yibeichen@ucsb.edu>
2022-07-18 12:22:02 -0700

    test failed ../pydra/pydra/engine/specs.py:490 exception

--- pydra/tasks/fsl/model/tests/test_run_flameo.py
+++ pydra/tasks/fsl/model/tests/test_run_flameo.py
@@ -6,7 +6,23 @@ from ..flameo import FLAMEO
 @pytest.mark.xfail(
     "FSLDIR" not in os.environ, reason="no FSL found", raises=FileNotFoundError
 )
-@pytest.mark.parametrize("inputs, outputs", [])
+@pytest.mark.parametrize(
+    "inputs, outputs",
+    [
+        (
+            {
+                "cope_file": "cope_merged.nii.gz",
+                "var_cope_file": "varcope_merged.nii.gz",
+                "cov_split_file": "design.grp",
+                "design_file": "design.mat",
+                "t_con_file": "design.con",
+                "mask_file": "mask.nii.gz",
+                "run_mode": "fe",
+            },
+            ["copes", "pes", "res4d", "var_copes", "tstats", "zstats"],
+        )
+    ],
+)
 def test_FLAMEO(test_data, inputs, outputs):
     if inputs is None:
         in_file = Path(test_data) / "test.nii.gz"
@@ -35,27 +51,3 @@ def test_FLAMEO(test_data, inputs, outputs):
     print("RESULT: ", res)
     for out_nm in outputs:
         assert getattr(res.output, out_nm).exists()
-
-
-@pytest.mark.parametrize("inputs, error", [(None, "AttributeError")])
-def test_FLAMEO_exception(test_data, inputs, error):
-    if inputs is None:
-        in_file = Path(test_data) / "test.nii.gz"
-        task = FLAMEO(in_file=in_file)
-    else:
-        for key, val in inputs.items():
-            try:
-                pattern = r"\.[a-zA-Z]*"
-                if isinstance(val, str):
-                    if re.findall(pattern, val) != []:
-                        inputs[key] = Path(test_data) / val
-                elif isinstance(val, list):
-                    if all(re.findall(pattern, _) != [] for _ in val):
-                        inputs[key] = [Path(test_data) / _ for _ in val]
-                else:
-                    inputs[key] = eval(val)
-            except:
-                pass
-        task = FLAMEO(**inputs)
-    with pytest.raises(eval(error)):
-        task.generated_output_names
