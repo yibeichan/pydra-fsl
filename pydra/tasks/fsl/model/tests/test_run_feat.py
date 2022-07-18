4145250cae81f9e83a6b6afd6a1bd1713e05b5a2
yibeichan <yibeichen@ucsb.edu>
2022-07-18 11:09:37 -0700

    fail the test if specify the output, pass the test if set AttributeErrors

--- pydra/tasks/fsl/model/tests/test_run_feat.py
+++ pydra/tasks/fsl/model/tests/test_run_feat.py
@@ -1,4 +1,4 @@
-import os, pytest
+import re, os, pytest
 from pathlib import Path
 from ..feat import FEAT
 
@@ -6,7 +6,7 @@ from ..feat import FEAT
 @pytest.mark.xfail(
     "FSLDIR" not in os.environ, reason="no FSL found", raises=FileNotFoundError
 )
-@pytest.mark.parametrize("inputs, outputs", [])
+@pytest.mark.parametrize("inputs, outputs", [({"fsf_file": "test.fsf"}, ["feat_dir"])])
 def test_FEAT(test_data, inputs, outputs):
     if inputs is None:
         in_file = Path(test_data) / "test.nii.gz"
@@ -14,8 +14,15 @@ def test_FEAT(test_data, inputs, outputs):
     else:
         for key, val in inputs.items():
             try:
-                if "file" in key:
-                    inputs[key] = Path(test_data) / val
+                pattern = r"\.[a-zA-Z]*"
+                if isinstance(val, str):
+                    if re.findall(pattern, val) != []:
+                        inputs[key] = Path(test_data) / val
+                    else:
+                        inputs[key] = eval(val)
+                elif isinstance(val, list):
+                    if all(re.findall(pattern, _) != [] for _ in val):
+                        inputs[key] = [Path(test_data) / _ for _ in val]
                 else:
                     inputs[key] = eval(val)
             except:
@@ -28,22 +35,3 @@ def test_FEAT(test_data, inputs, outputs):
     print("RESULT: ", res)
     for out_nm in outputs:
         assert getattr(res.output, out_nm).exists()
-
-
-@pytest.mark.parametrize("inputs, error", [(None, "AttributeError")])
-def test_FEAT_exception(test_data, inputs, error):
-    if inputs is None:
-        in_file = Path(test_data) / "test.nii.gz"
-        task = FEAT(in_file=in_file)
-    else:
-        for key, val in inputs.items():
-            try:
-                if "file" in key:
-                    inputs[key] = Path(test_data) / val
-                else:
-                    inputs[key] = eval(val)
-            except:
-                pass
-        task = FEAT(**inputs)
-    with pytest.raises(eval(error)):
-        task.generated_output_names
